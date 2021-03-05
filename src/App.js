20cda107f981b29f1e878419aca1ac4ae525f512
Danil <daniklucky38@gmail.com>
2021-03-05 16:11:54 +0400

    fix: style

--- src/App.js
+++ src/App.js
@@ -1,92 +1,109 @@
-import React, {Component} from 'react'
+import React, { Component } from "react";
 
-import Info from './components/info/info';
-import Form from './components/form/form';
-import Weather from './components/weather_info/weather_info';
+import Info from "./components/info/info";
+import Form from "./components/form/form";
+import Weather from "./components/weather_info/weather_info";
+
+import videoBg from "./img/video.mp4";
 
 const API_KEY = "8b0655f858009fd788bf700ca7e9a427";
 
 class App extends Component {
+  state = {
+    temp: undefined,
+    city: undefined,
+    country: undefined,
+    pressure: undefined,
+    sunrise: undefined,
+    sunset: undefined,
+    error: undefined,
+  };
 
-    state = {
+  gettingWeather = async (e) => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+
+    if (city) {
+      const api_url = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+      );
+      const data = await api_url.json();
+
+      let sunset = data.sys.sunset;
+      let date = new Date();
+      date.setTime(sunset);
+      let sunset_date =
+        date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+
+      let sunrise = data.sys.sunrise;
+      let date_sunrise = new Date();
+      date_sunrise.setTime(sunrise);
+      let sunrise_date_sunrise = date_sunrise.toLocaleString("en-US", {
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+        hour12: true,
+      });
+
+      this.setState({
+        temp: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        pressure: data.main.pressure,
+        sunrise: sunrise_date_sunrise,
+        sunset: sunset_date,
+        error: undefined,
+      });
+    } else {
+      this.setState({
         temp: undefined,
         city: undefined,
         country: undefined,
         pressure: undefined,
         sunrise: undefined,
         sunset: undefined,
-        error: undefined
+        error: "Введите название города",
+      });
     }
+  };
 
-    gettingWeather = async (e) => {
-        e.preventDefault();
-        const city = e.target.elements.city.value;
+  render() {
+    return (
+        <div className="main__intro">
+        <div className="intro">
+          <div className="video">
+            <video className="video__media" autoPlay muted loop>
+              <source src={videoBg} type="video/mp4" />
+            </video>
+          </div>
+        </div>
         
-        if(city){
-            const api_url = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
-            const data = await api_url.json();
-
-            let sunset = data.sys.sunset;
-            let date = new Date();
-            date.setTime(sunset);
-            let sunset_date = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
-
-            let sunrise = data.sys.sunrise;
-            let date_sunrise = new Date();
-            date_sunrise.setTime(sunrise);
-            let sunrise_date_sunrise = date_sunrise.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true })
-
-
-
-            this.setState({
-                temp: data.main.temp,
-                city: data.name,
-                country: data.sys.country,
-                pressure: data.main.pressure,
-                sunrise: sunrise_date_sunrise,
-                sunset: sunset_date,
-                error: undefined
-            });
-        } else {
-            this.setState({
-                temp: undefined,
-                city: undefined,
-                country: undefined,
-                pressure: undefined,
-                sunrise: undefined,
-                sunset: undefined,
-                error: "Введите название города"
-            })
-        }
-    }
-
-    render() {
-        return (
-            <div className="wrapper">
-                <div className="main">
-                    <div className="container">
-                        <div className="row ">
-                            <div className = "col-sm-5 info ">
-                                <Info/>
-                            </div>
-                            <div className = "col-sm-7 form">
-                                <Form weatherMethod={this.gettingWeather}/>
-                                <Weather
-                                    temp={this.state.temp}
-                                    city={this.state.city}
-                                    country={this.state.country}
-                                    pressure={this.state.pressure}
-                                    sunrise={this.state.sunrise}
-                                    sunset={this.state.sunset}
-                                    error={this.state.error}
-                                />
-                            </div>
-                        </div>
-                    </div>
-                </div>
+      <div className="wrapper">
+        <div className="main">
+          <div className="container">
+            <div className="row ">
+              <div className="col-sm-5 info ">
+                <Info />
+              </div>
+              <div className="col-sm-7 form">
+                <Form weatherMethod={this.gettingWeather} />
+                <Weather
+                  temp={this.state.temp}
+                  city={this.state.city}
+                  country={this.state.country}
+                  pressure={this.state.pressure}
+                  sunrise={this.state.sunrise}
+                  sunset={this.state.sunset}
+                  error={this.state.error}
+                />
+              </div>
             </div>
-        )
-    }
+          </div>
+        </div>
+      </div>
+      </div>
+    );
+  }
 }
 
 export default App;
